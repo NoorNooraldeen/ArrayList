4ac22e2f5a4a682d1e948ae793de32438aa108a7
NoorN <NoorN@NOOR>
2024-12-22 14:55:53 +0300

    Add project files.

---
+++ ArrayList/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//part1
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+ArrayList fruit = new ArrayList();
+
+fruit.Add("apple");
+fruit.Add("bnana");
+fruit.Add("cherry");
+
+
+Console.WriteLine("iteams");
+foreach(var item in fruit)
+{
+    Console.WriteLine(item);
+}
+fruit.Insert(2, "orang");
+fruit.RemoveAt(2);
+string search = Console.ReadLine();
+if (fruit.Contains(search)== true)
+{
+    Console.WriteLine($" avalible.{ search}");
+}
+else
+{
+    Console.WriteLine("ot avalible.");
+}
+{ 
+Console.WriteLine("enter an elemnt ");
+string elemnt = Console.ReadLine();
+int index = fruit.IndexOf(elemnt);
+Console.WriteLine($"index: {index}");
+}
+
+Console.WriteLine($" number of elemnt is  {fruit.Count}");
+
+
+//bounes 
